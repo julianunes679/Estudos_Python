036dfa15993ab7a691587a8ca763dd9d75e6d55f
julianunes679 <juliaalvesnunes1003@gmail.com>
2025-11-03 16:07:51 -0300

    Update FuncoeseMetodos.py

--- FuncoeseMetodos.py
+++ FuncoeseMetodos.py
@@ -6,3 +6,17 @@ saudacao()
 numeros = [1, 2, 3]
 numeros.append(6)  # adiciona um item
 print(numeros)
+
+
+### Escopo
+
+x = 10
+def minha_funcao():
+    x = 5
+    print ("dentro:", x)
+
+minha_funcao()
+print ("fora:", x)
+
+
+### Global
